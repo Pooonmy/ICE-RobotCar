4a2cd9a4f40919891cbee981575951345c0f24f1
Pisit Pankewjana <pisit.poon.45@gmail.com>
2025-10-02 22:30:46 +0700

    Update v1.ino

--- v1/v1.ino
+++ v1/v1.ino
@@ -5,6 +5,8 @@ UTFT tft(ST7735, 6, 7, 3, 4, 5);
 #define White VGA_WHITE
 #define Black VGA_BLACK
 
+#define btnPin 0
+#define buzzPin 19
 #define trigPin 1
 #define echoPin 2
 const unsigned long PULSE_TIMEOUT_US = 30000UL;
@@ -17,77 +19,37 @@ const int irPins[numIR] = { A0, A1, A2, A3, A4 };
 int irVals[numIR];
 
 long duration;
-int distanceCm;
+int distance;
 
-int readUltrasonicCM() {
-  // Trigger 10 µs pulse
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Measure echo pulse
-  unsigned long duration = pulseIn(echoPin, HIGH, PULSE_TIMEOUT_US);
-  if (duration == 0) return -1;  // timeout / no echo
-
-  // Convert time->distance (round trip: divide by 2)
-  int cm = (int)((duration / 2.0f) / SOUND_US_PER_CM);
-
-  if (cm < MIN_CM || cm > MAX_CM) return -1;
-  return cm;
-}
-
-// --- Median of n (odd) samples to reduce noise ---
-int readUltrasonicCM_Median(uint8_t n = 5) {
-  if (n < 3 || (n % 2) == 0) n = 5;  // enforce odd >=3
-  int vals[9];                       // supports up to 9; increase if you need larger n
-  if (n > 9) n = 9;
-
-  uint8_t count = 0;
-  for (uint8_t i = 0; i < n; i++) {
-    int v = readUltrasonicCM();
-    if (v != -1) vals[count++] = v;
-    delay(10);  // brief settle between pings
-  }
-  if (count == 0) return -1;
-
-  // simple insertion sort
-  for (uint8_t i = 1; i < count; i++) {
-    int key = vals[i], j = i;
-    while (j > 0 && vals[j - 1] > key) {
-      vals[j] = vals[j - 1];
-      j--;
-    }
-    vals[j] = key;
-  }
-  return vals[count / 2];
-}
-
-void readIR() {
-  for (int i = 0; i < numIR; i++) {
-    irVals[i] = analogRead(irPins[i]);
-  }
-}
+void beep(int count);
 
 void setup() {
   tft.InitLCD();
   tft.setFont(SmallFont);
   tft.clrScr();
   tft.fillScr(Black);
+  tft.print("Press to start", CENTER, 56);
 
+  pinMode(btnPin, INPUT_PULLUP);
+  pinMode(buzzPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  digitalWrite(buzzPin, HIGH);
   digitalWrite(trigPin, LOW);
-
   for (int i = 0; i < numIR; i++) {
     pinMode(irPins[i], INPUT);
   }
+
+  while (digitalRead(btnPin) == HIGH) {
+    // Do absolutely nothing
+  }
+
+  beep(1);
+  tft.clrScr();
 }
 
 void loop() {
-  int distance = readUltrasonicCM();
-  // int distance = readUltrasonicCM_Median(5);
+  readUltrasonic();
   readIR();
   tft.setColor(White);
   tft.print(String("Distance:"), 0, 0);
@@ -97,5 +59,37 @@ void loop() {
     tft.print(String(i), 24, 12 + 12 * i);
     tft.print(String(":"), 32, 12 + 12 * i);
     tft.print(String(irVals[i]) + "    ", 48, 12 + 12 * i);
+  }
+}
+
+void readUltrasonic() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  unsigned long duration = pulseIn(echoPin, HIGH, PULSE_TIMEOUT_US);
+  if (duration == 0) return -1;
+
+  int cm = (int)((duration / 2.0f) / SOUND_US_PER_CM);
+
+  if (cm < MIN_CM || cm > MAX_CM) return -1;
+  
+  distance = cm;
+}
+
+void readIR() {
+  for (int i = 0; i < numIR; i++) {
+    irVals[i] = analogRead(irPins[i]);
+  }
+}
+
+void beep(int count) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(buzzPin, LOW);
+    delay(150);
+    digitalWrite(buzzPin, HIGH);
+    delay(150);
   }
 }
